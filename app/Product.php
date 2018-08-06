78bc92b759ef756adf412b188a75e34d7895353b
System Administrator <root@Ashiss-MacBook-Air.local>
2018-08-06 17:49:27 +0530

    Commented Elasticsearch code in Model Product

--- app/Product.php
+++ app/Product.php
@@ -4,12 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Elasticquent\ElasticquentTrait;
+// use Elasticquent\ElasticquentTrait;
 
 class Product extends Model
 {
     use SoftDeletes;
-    use ElasticquentTrait;
+    // use ElasticquentTrait;
     /**
      * The database table used by the model.
      *
@@ -36,7 +36,7 @@ class Product extends Model
      *
      * @var array
      */
-    protected $mappingProperties = array('name' => array('type' => 'text','analyzer' => 'standard'),'category' => array('type' => 'text','analyzer' => 'standard'));
+    // protected $mappingProperties = array('name' => array('type' => 'text','analyzer' => 'standard'),'category' => array('type' => 'text','analyzer' => 'standard'));
 
 
     /**
@@ -44,47 +44,47 @@ class Product extends Model
      *
      * @var array
      */
-    protected $indexSettings = [
-        'analysis' => [
-            'char_filter' => [
-                'replace' => [
-                    'type' => 'mapping',
-                    'mappings' => [
-                        '&=> and '
-                    ],
-                ],
-            ],
-            'filter' => [
-                'word_delimiter' => [
-                    'type' => 'word_delimiter',
-                    'split_on_numerics' => false,
-                    'split_on_case_change' => true,
-                    'generate_word_parts' => true,
-                    'generate_number_parts' => true,
-                    'catenate_all' => true,
-                    'preserve_original' => true,
-                    'catenate_numbers' => true,
-                ]
-            ],
-            'analyzer' => [
-                'default' => [
-                    'type' => 'custom',
-                    'char_filter' => [
-                        'html_strip',
-                        'replace',
-                    ],
-                    'tokenizer' => 'whitespace',
-                    'filter' => [
-                        'lowercase',
-                        'word_delimiter',
-                    ],
-                ],
-            ],
-        ],
-        'index'=> [
-            "max_result_window" => 500000
-        ],
-    ];
+    // protected $indexSettings = [
+    //     'analysis' => [
+    //         'char_filter' => [
+    //             'replace' => [
+    //                 'type' => 'mapping',
+    //                 'mappings' => [
+    //                     '&=> and '
+    //                 ],
+    //             ],
+    //         ],
+    //         'filter' => [
+    //             'word_delimiter' => [
+    //                 'type' => 'word_delimiter',
+    //                 'split_on_numerics' => false,
+    //                 'split_on_case_change' => true,
+    //                 'generate_word_parts' => true,
+    //                 'generate_number_parts' => true,
+    //                 'catenate_all' => true,
+    //                 'preserve_original' => true,
+    //                 'catenate_numbers' => true,
+    //             ]
+    //         ],
+    //         'analyzer' => [
+    //             'default' => [
+    //                 'type' => 'custom',
+    //                 'char_filter' => [
+    //                     'html_strip',
+    //                     'replace',
+    //                 ],
+    //                 'tokenizer' => 'whitespace',
+    //                 'filter' => [
+    //                     'lowercase',
+    //                     'word_delimiter',
+    //                 ],
+    //             ],
+    //         ],
+    //     ],
+    //     'index'=> [
+    //         "max_result_window" => 500000
+    //     ],
+    // ];
 
     
 }
